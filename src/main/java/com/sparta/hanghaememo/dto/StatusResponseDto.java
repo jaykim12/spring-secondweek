8cf362f52888e8a4f368eaf07e42e9df04523594
kes <kmskes1125@gmail.com>
2023-04-23 00:31:35 +0900

    StatusResponseDto.java 커밋
    메시지, 상태코드를 반환 해 주는 StatusResponseDto.java 커밋함

---
+++ src/main/java/com/sparta/hanghaememo/dto/StatusResponseDto.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaememo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class StatusResponseDto{
+    private String msg;
+    private HttpStatus status;
+}
