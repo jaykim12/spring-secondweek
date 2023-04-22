c049c2c9890e98fbaf026fae400f705c09093a49
kes <kmskes1125@gmail.com>
2023-04-23 00:32:34 +0900

    UserRequestDto.java 커밋
    controller 계층에서 login api에서 클라이언트에게 입력받는 UserRequestDto.java 커밋함

---
+++ src/main/java/com/sparta/hanghaememo/dto/UserRequestDto.java
@@ -0,0 +1,17 @@
+package com.sparta.hanghaememo.dto;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserRequestDto {
+
+    @Pattern(regexp = "^[0-9a-z]{4,10}$", message = "4 ~ 10자 사이의 알파벳 소문자와 숫자만 가능합니다.")
+    private String username;
+
+    @Pattern(regexp = "^[0-9a-zA-Z]{8,15}$", message = "8 ~ 15자 사이의 알파벳 대소문자와 숫자만 가능합니다.")
+    private String password;
+
+}
