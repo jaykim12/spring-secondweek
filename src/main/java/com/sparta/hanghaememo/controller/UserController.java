8a28eba01a06b66eaa5b883313feca7dccc69fb4
kes <kmskes1125@gmail.com>
2023-04-23 00:29:59 +0900

    UserController.java 커밋
    login 기능 api가 구현되어 있는 UserController 커밋함

---
+++ src/main/java/com/sparta/hanghaememo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.sparta.hanghaememo.controller;
+
+import com.sparta.hanghaememo.dto.UserRequestDto;
+import com.sparta.hanghaememo.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import com.sparta.hanghaememo.dto.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public StatusResponseDto signup(@Valid @RequestBody UserRequestDto userRequestDto) {
+        return userService.signup(userRequestDto);
+    }
+
+}
