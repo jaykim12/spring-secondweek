3113d529f30ecfc9582781855a57257cad565030
kes <kmskes1125@gmail.com>
2023-04-24 16:19:19 +0900

    Memo.java 커밋
    username에 unique = true를 추가함

--- src/main/java/com/sparta/hanghaememo/entity/Memo.java
+++ src/main/java/com/sparta/hanghaememo/entity/Memo.java
@@ -4,8 +4,10 @@ import com.sparta.hanghaememo.dto.MemoRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 public class Memo extends Timestamped {
@@ -13,8 +15,8 @@ public class Memo extends Timestamped {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false) //이게 받는 값
-    private String username;
+//    @Column(nullable = false) //이게 받는 값
+//    private String username;
 
     @Column(nullable = false) //이게 받는 값
     private String contents;
@@ -22,22 +24,22 @@ public class Memo extends Timestamped {
     @Column(nullable = false) //이게 받는 값
     private String title;
 
-    @Column(nullable = false) //이게 받는 값
-    private String password;
+    @ManyToOne(fetch = FetchType.LAZY) //이게 받는 값
+    @JoinColumn(name = "username")
+    private User user;
 
     //MemoService에서 memoCreate 메서드가 requestDto를 인자로 전달하면
     // 해당 매개변수를 이용해 memo를 초기화 한다.
     public Memo(MemoRequestDto requestDto) {
-        this.username = requestDto.getUsername();
+//        this.username = requestDto.getUsername();
         this.contents = requestDto.getContents();
         this.title = requestDto.getTitle();
-        this.password = requestDto.getPassword();
     }
 
     //MemoService.updateMemo 메서드를 사용할 때 인자로 전달되는 requestDto를
     //받아 메모를 수정하기 위한 메서드
     public void update(MemoRequestDto requestDto) {
-        this.username = requestDto.getUsername();
+//        this.username = requestDto.getUsername();
         this.contents = requestDto.getContents();
         this.title = requestDto.getTitle();
     }
