e9773c1ecc876612a6deb50173b6827500c1443e
kes <kmskes1125@gmail.com>
2023-04-24 16:23:35 +0900

    UserService.java 커밋
    로그인 기능을 추가함

--- src/main/java/com/sparta/hanghaememo/service/UserService.java
+++ src/main/java/com/sparta/hanghaememo/service/UserService.java
@@ -6,6 +6,7 @@ import com.sparta.hanghaememo.entity.User;
 import com.sparta.hanghaememo.jwt.JwtUtil;
 import com.sparta.hanghaememo.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
+import jdk.jshell.Snippet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -34,4 +35,23 @@ public class UserService {
         return new StatusResponseDto("회원가입을 성공적으로 마쳤습니다.", HttpStatus.OK);
     }
 
+    @Transactional
+    public StatusResponseDto login(UserRequestDto userRequestDto, HttpServletResponse response) {
+        String username = userRequestDto.getUsername();
+        String password = userRequestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        // 비밀번호 확인
+        if(!user.getPassword().equals(password)){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        //http header에 jwt 토큰 발급
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+
+        return new StatusResponseDto("로그인이 완료되었습니다.", HttpStatus.OK);
+    }
 }
