ed5ff6b4be2bf01c7c5c195af566a473d814667d
kes <kmskes1125@gmail.com>
2023-04-24 16:20:13 +0900

    User.java 커밋
    username에 unique = true를 추가함

--- src/main/java/com/sparta/hanghaememo/entity/User.java
+++ src/main/java/com/sparta/hanghaememo/entity/User.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
